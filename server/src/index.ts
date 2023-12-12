6f0d4f5e1b2eef3d7cfb25dd3d9b0f121945f111
pacexy <pacexy@gmail.com>
2023-12-12 08:42:57 +0800

    Expose web server (#1)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,40 @@
-import express from 'express';
-import {execSync} from 'child_process';
-import cors from 'cors';
+import express from 'express'
+import { execSync } from 'child_process'
+import cors from 'cors'
 
 export const segment = (text: string) => {
   try {
-    return JSON.parse(execSync(`cd /root/quicklisp/local-projects/ichiran/ && ./ichiran-cli -f -- "${text}"`).toString());
+    return JSON.parse(
+      execSync(
+        `cd /root/quicklisp/local-projects/ichiran/ && ./ichiran-cli -f -- "${text}"`
+      ).toString()
+    )
   } catch (e) {
-    return [];
+    console.error(e)
+    return []
   }
 }
-
-(async () => {
+;(async () => {
   //await build();
 
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
   app.post('/segmentation', (req, res) => {
     if (req.body.text && req.body.text.length > 0) {
-      res.json(segment(req.body.text));
+      res.json(segment(req.body.text))
     } else {
-      res.status(500);
-      res.send();
+      res.status(500)
+      res.send()
     }
-  });
-
-  app.listen(80, () => {
-    console.log('Started server');
   })
-})();
 
+  // Use PORT provided in environment or default to 3000
+  const portEnv = process.env.PORT
+  const port = portEnv ? parseInt(portEnv) : 3000
 
-
-
+  // Listen on `port` and 0.0.0.0
+  app.listen(port, '0.0.0.0', function () {
+    console.log(`Server started on port ${port}!`)
+  })
+})()
